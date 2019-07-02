9e8248ba607726e8ff1a6628645b216d3620ebe6
filipswistowski <filipswistowski@gmail.com>
2019-07-02 13:16:39 +0200

    Add basic classes

---
+++ src/main/java/com/codecool/xdvirus/onlineShop/Category.java
@@ -0,0 +1,23 @@
+package com.codecool.xdvirus.onlineShop;
+import java.util.List;
+
+public class Category {
+
+    private int id;
+    private String name;
+    private boolean isAvailable;
+    private List<Product> products;
+
+    public String Category() {
+
+        return this.name;
+    }
+
+    public List<Product> getProducts() {
+
+        return products;
+    }
+
+
+}
+
