1ef7b5512e51e4e535309b7af7b77192843c6e76
patryczkov <d1metyloam1na>
2019-07-04 10:01:53 +0200

    Add User constructor and getter.

--- src/main/java/com/codecool/xdvirus/onlineShop/model/User.java
+++ src/main/java/com/codecool/xdvirus/onlineShop/model/User.java
@@ -1,4 +1,36 @@
 package com.codecool.xdvirus.onlineShop.model;
 
 public class User {
+    private int id;
+    private String login;
+    private String password;
+    private int permission;
+
+    public User(int id, String login, String password, int permission) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.permission = permission;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getPermission() {
+        return permission;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " " + this.login + " " + this.password + " " + this.permission;
+    }
 }
